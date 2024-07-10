12a10587f01b502c21edf99bb7c00c2ff95bd715
FerJarquin <mjarquin@est.utn.ac.cr>
2024-07-09 22:46:00 -0600

    Form Servicios

--- src/app/pages/servicios/servicios.component.ts
+++ src/app/pages/servicios/servicios.component.ts
@@ -2,18 +2,35 @@ import { Component, signal } from '@angular/core';
 import { Servicio } from '../../model/servicios';
 import { JsonPipe } from '@angular/common'
 import { HttpClient } from '@angular/common/http';
+import {FormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-servicios',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe, FormsModule],
   templateUrl: './servicios.component.html',
   styleUrl: './servicios.component.css'
 })
 export class ServiciosComponent {
 
   public Titulo = "Administracion de servicios";
-  public Servicios = signal<Servicio[]>([]);
+
+  public servicioId: String = '';
+  public nombreServicio: String = '';
+  public descripcionServicio: String = '';
+  public empleadoId: String = '';
+
+
+  printInputs() {
+    console.log('servicioId:', this.servicioId);
+    console.log('nombreServicio:', this.nombreServicio);
+    console.log('descripcionServicio:', this.descripcionServicio);
+    console.log('empleadoId:', this.empleadoId);    
+  }
+
+  public Servicios = signal<Servicio[]>([]); 
+  
+
 
   constructor(private http: HttpClient) {
     this.metodoGETServicios();
@@ -50,5 +67,26 @@ export class ServiciosComponent {
     , EmpleadoId: EmpleadoId
   };
   this.Servicios.update((Servicios) => [...Servicios, nuevaServicio]);
-};
+  };
+
+ public agregarServicio(event:  Event) {
+    let tag = event.target as HTMLInputElement
+    let cuerpo = {
+      NombreServicio: this.nombreServicio,
+      DescripcionServicio:  this.descripcionServicio,
+      EmpleadoId: 6 //this.empleadoId
+    }
+    this.http.post('http://localhost/servicios/', cuerpo).subscribe(
+      () => {
+      //this.Usuarios.update((Usuarios) => [...Usuarios, cuerpo]);
+    }
+  );
+  };
+
+
+  public borrarServicio() {
+    this.http.delete('http://localhost/servicios/' + this.servicioId).subscribe(() => {
+     // this.Usuarios.update(Usuarios) => Usuarios.filter((Usuario) => Usuario.UsuarioId !== this.usuarioId));
+    });
+  };
 }
